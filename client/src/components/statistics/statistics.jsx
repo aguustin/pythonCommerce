4fe9f1fb365714d2b6e758e27a5104c222338631
unknown <agustin.molee@gmail.com>
2024-06-04 20:56:00 -0300

    filtrando por fechas

--- client/src/components/statistics/statistics.jsx
+++ client/src/components/statistics/statistics.jsx
@@ -20,7 +20,11 @@ const Statistics = () => {
     const [salesStats, setSalesStats] = useState()
     const [totalSales, settotalSales] = useState(0)
     const [buys, setBuys] = useState()
-
+    const [seeDetails, setSeeDetails] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [dateFrom, setDateFrom] = useState('');
+    const [dateTo, setDateTo] = useState('');
+    
             useEffect(() => {
                 fetch('http://127.0.0.1:8000/getAllProducts/') //  fetch('https://fakestoreapi.com/products 'http://127.0.0.1:8000/getAllProducts/'')
                 .then(res=>res.json())
@@ -31,7 +35,7 @@ const Statistics = () => {
                 .then(json=>setBuys(json))
             // eslint-disable-next-line react-hooks/exhaustive-deps
             },[])
-            console.log(buys)
+            
             useEffect(() => {
                 const profitsArray = products.map(product => Math.round(product.profits));
                 setProfitsStats(profitsArray);
@@ -56,10 +60,64 @@ const Statistics = () => {
     
     ChartJS.register(CategoryScale);
 
+    const filteredProducts = products.filter(product =>
+        new RegExp(searchTerm, 'i').test(product.productName)
+    );
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value); // Update search term state
+    };
+
+    const onDateFrom = (e) => {
+        setDateFrom(e.target.value)
+    }
+
+    const onDateTo = (e) => {
+        setDateTo(e.target.value)
+    }
+
+    const openStatistics = () => {
+        document.getElementById("charts").style.position = "fixed";
+        document.getElementById("charts").style.top = "80px"
+        document.getElementById("charts").style.bottom = "0"
+        document.getElementById("charts").style.right = "0"
+        document.getElementById("charts").style.left = "16.5vw"
+        document.getElementById("charts").style.width = "100%"
+        document.getElementById("charts").style.overflowY = "scroll"
+        setSeeDetails(true)
+        console.log('productos', products)
+    }
+
+    const openProductStatistics = () => {
+
+        if(document.getElementById("stat").style.height == "90vh"){
+            document.getElementById("stat").style.width = "41.40vw"
+            document.getElementById("stat").style.height = "38.90vh"
+        }else{
+            document.getElementById("stat").style.position = 
+            document.getElementById("stat").style.width = "80vw"
+            document.getElementById("stat").style.height = "90vh"
+            document.getElementById("stat").style.transition = "all .5s"
+        }
+    }
+
+    const staticsFromDates = (e) => {
+        e.preventDefault()
+        const dates = {
+            dateStart: e.target.elements.dateStart.value,
+            dateEnd: e.target.elements.dateEnd.value
+        }
+
+        console.log(dates);
+    }
+
     return(
         <>
         <div className='statistics'>
             <nav className="side-nav">
+            <form>
+                <input type="text" name="search" value={searchTerm} onChange={handleSearchChange}></input>
+            </form>
                 <ul>
                     <li><button>Profits</button></li>
                     <li><button>Losses</button></li>
@@ -99,10 +157,73 @@ const Statistics = () => {
                     </div>
                 </section>
                 <section className='chart-recents-sales-container'>
-                    <div className='chart'>
-                        <Bar
+                    <div className="chart" id="charts">
+                       {seeDetails
+                       ?     
+                       <div className='stat-detail'>
+                       {filteredProducts.map((p) => {
+                            return(
+                                <>
+                                <div className='stat' id='stat'>
+                                <form onSubmit={(e) => staticsFromDates(e)}>
+                                    <label htmlFor="start">From:</label>
+                                    <input type="date" id="start" name="dateStart" value={dateFrom} min="2020-05-10" max="2024-07-00" onChange={onDateFrom} />
+                                    <label htmlFor="start">To:</label>
+                                    <input type="date" id="end" name="dateEnd" value={dateTo} min="2020-05-10" max="2024-07-00" onChange={onDateTo} />
+                                    <button className='stats-button' type='submit'>Stats</button>
+                                </form>
+                                <label>{p.productName}</label>
+                                <Bar key={p.id} id="abc" onClick={() => openProductStatistics()}          
+                                        data={{
+                                            labels: ['January'],
+                                            datasets: [
+                                                {
+                                                    label: "Profits",
+                                                    data: [p.profits],
+                                                    borderColor: '#36A2EB',
+                                                    backgroundColor: '#5afd68',
+                                                },
+                                                {
+                                                    label: "Losses",
+                                                    data: [p.losses],
+                                                    borderColor: '#36A2EB',
+                                                    backgroundColor: '#ff6464',
+                                                },
+                                                {
+                                                    label: "Stock",
+                                                    data: [p.quantity],
+                                                    borderColor: '#36A2EB',
+                                                    backgroundColor: '#649aff',
+                                                },
+                                                {
+                                                    label: "Sales",
+                                                    data: [p.sales],
+                                                    borderColor: '#36A2EB',
+                                                    backgroundColor: '#ff64b9',
+                                                    
+                                                }
+                                            ],
+                                            options: {
+                                                     scales: {
+                                                        y: {
+                                                            min: 2,
+                                                            max: 1000000,
+                                                        }
+                                                    }
+                                                    }  
+                                    
+                                        }}
+                                        
+                                />
+                                </div>
+                                </>
+                            )
+                        })}
+                         </div>
+                       :
+                       <Bar
                             data={{
-                                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                                labels: ['January'],
                                 datasets: [
                                     {
                                         label: "Profits",
@@ -131,6 +252,9 @@ const Statistics = () => {
                                 ]  
                             }}
                          />
+                       } 
+                         
+                    <button id="see-details" onClick={() =>openStatistics()}>See details</button>
                     </div>
                     <div className='recents-sales'>
                         <div>
